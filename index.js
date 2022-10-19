308955b25fa0d945a6bdc551e228c2f44a78d17e
dstoffels <dan.stoffels@gmail.com>
2022-10-19 14:48:56 -0500

    init base files

---
+++ index.js
@@ -0,0 +1,23 @@
+////////////////// OBJECTIVE //////////////////
+// Recreate the elements of the CARA CARA site using only DOM manipulation in JavaScript.
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Manipulating_documents
+
+// TASK 1 //
+// Add a "root" div in the body of your html file
+// Access the root from your js file with document.getElementById()
+
+// TASK 2 //
+// Create a header element using document.createElement()
+// Append the new header element as a child of the root div with the appendChild method
+// of the root.
+
+// TASK 3 //
+// Create a pure function that will create and return a new DOM element,
+// but also allow you to assign its parent element, className, and id.
+// Parameters: (element, parent, className='', id='')
+
+// TASK 4 //
+// Create an unordered list element inside of the header
+// Create all four nav items (home, about, products, contact)
+// using a list of strings and your new function.
+// Take some time to style the list so the <li></li> elements are inline and have no bullets.
